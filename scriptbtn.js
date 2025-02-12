40fa57b84256cbdd5275c74c4019e2d9b757b9a4
ROCKNITE-STUDIO <rocknite.studio@gmail.com>
2025-02-12 16:29:38 +0100

    Delete scriptbtn.js

--- scriptbtn.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btnAdd = document.getElementById('btn-add');
-
-    if (btnAdd) {
-        btnAdd.addEventListener('click', function() {
-            window.location.href = 'new';
-        });
-    }
-});
